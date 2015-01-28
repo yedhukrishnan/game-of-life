d1bd3d9d370c81752b99072a3d5abcfd2c48d92a
Yedhu Krishnan <yedhu_krishnan@yahoo.com>
2015-01-28 08:55:12 +0530

    Add basic rules

--- ruby/lib/game_of_life.rb
+++ ruby/lib/game_of_life.rb
@@ -1,5 +1,5 @@
 class GameOfLife
-
-  
-  
+  def alive_in_next_gen? neighbour_count, alive_in_this_gen
+    (alive_in_this_gen && neighbour_count == 2) || neighbour_count == 3
+  end
 end
--- ruby/spec/game_of_life_spec.rb
+++ ruby/spec/game_of_life_spec.rb
@@ -3,8 +3,25 @@ require './lib/game_of_life'
 
 RSpec.describe GameOfLife do
 
-  it "yet to implement" do
-    expect(true).to be_truthy
+  before(:example) do
+    @game_of_life = GameOfLife.new
+  end
+
+  it "live cell with fewer than two live neighbours dies" do
+    expect(@game_of_life.alive_in_next_gen?(1, true)).to be_falsy
+  end
+
+  it "live cell with two or three live neighbours lives on to the next generation" do
+    expect(@game_of_life.alive_in_next_gen?(2, true)).to be_truthy
+    expect(@game_of_life.alive_in_next_gen?(3, true)).to be_truthy
+  end
+
+  it "live cell with more than three live neighbours dies" do
+    expect(@game_of_life.alive_in_next_gen?(4, true)).to be_falsy
+  end
+
+  it "dead cell with exactly three live neighbours becomes a live cell" do
+    expect(@game_of_life.alive_in_next_gen?(3, false)).to be_truthy
   end
   
 end
